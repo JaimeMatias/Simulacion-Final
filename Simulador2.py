dd58b21afcf092440defb4de03448c5aec49f6d2
JaimeMatias <matias.raul.jaime@gmail.com>
2018-06-23 15:16:16 -0300

    Simulador 1 y 2

--- Simulador2.py
+++ Simulador2.py
@@ -2,12 +2,12 @@ import aleatorio
 CANTP=input("INGRESE LA CANTIDAD DE NUMEROS A GENERAR : ")
 CANTP=int(CANTP)
 contador=0
-archivo = open("Generador1.txt", "w")
+archivo = open("Generador2.txt", "w")
 archivo.write("Numeros: "+ '\n')
 while contador <= CANTP:
-    VARIABLE= aleatorio.aleatorio(0.006944,0.0013889)
+    VARIABLE= aleatorio.aleatorio(0.0031250,0.093750)
     print(VARIABLE)
     contador=contador+1
     archivo.write(str(VARIABLE)+ '\n')
 input("Numeros Generados, Presione Cualquier tecla")
-archivo.close()
+archivo.close
